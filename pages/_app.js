80636920b45d4201e70162e0dd84166be58b68bf
tereza-tech <tereza.tech@gmail.com>
2022-04-18 09:50:54 +0000

    commit changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+/**
+ * Using a custom _app.js with next-seo you can set default SEO
+ * that will apply to every page. Full info on how the default works
+ * can be found here: https://github.com/garmeeh/next-seo#default-seo-configuration
+ */
+import { DefaultSeo } from 'next-seo'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import SEO from '../next-seo.config'
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    /* Here we call NextSeo and pass our default configuration to it  */
+    <>
+      <DefaultSeo {...SEO} />
+      <Component {...pageProps} />
+    </>
+  )
 }
-
-export default MyApp
