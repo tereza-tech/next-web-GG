8aa4a1b4a6519e1bfba211edd871ebed0ef1fe75
Tereza Malenická <tereza.tech@gmail.com>
2022-05-26 10:55:09 +0200

    for merge

---
+++ components/i2contactUsComponent.tsx
@@ -0,0 +1,188 @@
+import React from 'react';
+import {
+  createStyles,
+  Text,
+  SimpleGrid,
+  TextInput,
+  Textarea,
+  Button,
+  Group,
+  ActionIcon,
+  Title,
+  CSSObject, MediaQuery, Container
+} from '@mantine/core';
+import { useForm } from '@mantine/form';
+import { BrandTwitter, BrandYoutube, BrandInstagram } from 'tabler-icons-react';
+import { ContactIconsList } from './ContactIcons';
+import { mockSubmit, FormData } from '../api/airtable';
+
+let form: FormData = {
+  "email": '',
+  "name": '',
+  "message": '',
+  "phone": '',
+  "callback": ''
+}
+
+interface BothContacts extends FormData {
+  mailaphone: void;
+}
+ 
+
+const useStyles = createStyles((theme) => ({
+  root: {
+   
+  },
+  wrapper: {
+    width: '100%',
+    backgroundColor: '#141517',
+    borderRadius: '16px',
+    minHeight: 400,
+    boxSizing: 'border-box',
+    backgroundImage: `linear-gradient(-60deg, ${theme.colors[theme.primaryColor][4]} 0%, ${
+      theme.colors[theme.primaryColor][7]
+    } 100%)`,
+    padding: theme.spacing.xl * 2.5,
+
+    [`@media (max-width: ${theme.breakpoints.sm}px)`]: {
+      padding: theme.spacing.xl * 1.5,
+    },
+  },
+
+  title: {
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+    color: theme.white,
+    lineHeight: 1,
+  },
+
+  description: {
+    color: theme.colors[theme.primaryColor][0],
+    maxWidth: 400,
+
+    [`@media (max-width: ${theme.breakpoints.sm}px)`]: {
+      maxWidth: '100%',
+    },
+  },
+
+  form: {
+    padding: theme.spacing.xl,
+    paddingTop: 0,
+    borderRadius: theme.radius.md,
+    boxShadow: theme.shadows.lg,
+  },
+
+  social: {
+    color: theme.white,
+
+    '&:hover': {
+      color: theme.colors[theme.primaryColor][1],
+    },
+  },
+
+  fields: {
+    marginTop: 28,
+  },
+
+  input: {
+    borderColor: theme.colors.gray[4],
+    color: theme.black,
+
+    '&::placeholder': {
+      color: theme.colors.gray[5],
+    },
+  },
+
+  inputLabel: {
+    color: theme.black,
+  },
+
+  control: {
+    backgroundColor: '#fede00',
+    color: 'black'
+  },
+}));
+
+const social = [BrandTwitter, BrandYoutube, BrandInstagram];
+
+export function ContactUs() {
+  const { classes } = useStyles();
+
+const smWWQuery: CSSObject = {
+  display: 'none'
+}
+
+const lgWWQuery: CSSObject = {
+  display: 'none'
+}
+
+  function Demo() {
+    const form = useForm<FormData>({
+      initialValues: {
+        email: '',
+        name: '',
+        message: '',
+        phone: '',
+        callback: ''
+      },
+      validate: {
+        name: (value) => (value.length < 4 ? 'Prosím uveďte Vaše jméno.' : null),
+        message: (value) => (value.length < 2 ? 'Prosíme vyplňte text zprávy.' : null),
+      },
+    });
+  }
+
+  const icons = social.map((Icon, index) => (
+    <ActionIcon key={index} size={28} className={classes.social} variant="transparent">
+      <Icon size={22} />
+    </ActionIcon>
+  ));
+
+  return (
+    <div className={classes.wrapper}>
+      <SimpleGrid cols={2} spacing={50} breakpoints={[{ maxWidth: 'sm', cols: 1 }]}>
+        <div>
+          <Title className={classes.title}>Poptávka</Title>
+          <Text className={classes.description} mt="sm" mb={30}>
+            Pošlete nám nezávaznou poptávku, my Vás do 24 hodin kontaktujeme nazpět.
+          </Text>
+
+          <ContactIconsList variant="white" />
+
+          <Group mt="xl">{icons}</Group>
+          <MediaQuery smallerThan="md" styles={smWWQuery}>
+          <Container>
+          <br /><br />
+          <br /><br />          <Title style={{color: '#fede00'}} order={4}>Which Way? This Way!</Title></Container></MediaQuery>
+        </div>
+        <form>
+       {/* <form className={classes.form} onSubmit={form.onSubmit((values) => console.log(values))}>*/}
+        
+
+          <div className={classes.fields}>
+            <SimpleGrid cols={2} breakpoints={[{ maxWidth: 'sm', cols: 1 }]}>
+              <TextInput label="Celé jméno" placeholder="Jmeéno Příjmenní" />
+              <TextInput label="Telefonní číslo" placeholder="+420 " required />
+            </SimpleGrid>
+            <TextInput mt="md" label="E-mail" placeholder="" required />
+            <TextInput mt="md" label="Máte preferovaný čas, kdy Vás kontaktovat?" placeholder="" />
+<Textarea
+              mt="md"
+              label="Prosíme specifikujte zde vaše požadavky:"
+              placeholder=""
+              minRows={3}
+              required
+            />
+            <Group position="right" mt="md">
+              <Button type="submit" className={classes.control}>
+                Odeslat
+              </Button>
+            </Group>
+          </div>
+        </form>
+        <MediaQuery largerThan="xs" styles={lgWWQuery}>
+          <Container><Title style={{color: '#fede00'}} order={4}>Which Way? This Way!</Title></Container></MediaQuery>
+       
+      </SimpleGrid>
+    </div>
+  );
+}
---
+++ components/togglableComponent.tsx
@@ -0,0 +1,136 @@
+import React, { useState } from 'react';
+import { Dialog, Group, Button, TextInput, Text, CSSObject, MediaQuery, Container } from '@mantine/core';
+import {
+  Paper,
+  Textarea,
+  ThemeIcon,
+  SimpleGrid,
+  createStyles,
+} from '@mantine/core';
+import { ContactIconsList } from './ContactIcons';
+import { Phone } from 'tabler-icons-react';
+
+const useStyles = createStyles((theme) => {
+  const BREAKPOINT = theme.fn.smallerThan('sm');
+  return {
+    wrapper: {
+      display: 'flex',
+      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.white,
+      borderRadius: theme.radius.lg,
+      padding: 4,
+      border: `1px solid ${
+        theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[2]
+      }`,
+
+      [BREAKPOINT]: {
+        flexDirection: 'column',
+      },
+    },
+    button: {
+      backgroundColor: '#fede00',
+      height: 48,
+      position: 'relative',
+      bottom: 280,
+      color: 'black', 
+      borderRadius: '14px 0 0 14px',
+      fontWeight: 600,
+    },
+    contacts: {
+      boxSizing: 'border-box',
+      top: 'calc(10%)',
+      borderRadius: theme.radius.lg - 2,
+      border: '1px solid transparent',
+      padding: theme.spacing.xl,
+      flex: '0 0 215px',
+
+      [BREAKPOINT]: {
+        marginBottom: theme.spacing.sm,
+        paddingLeft: theme.spacing.md,
+      },
+    },
+
+    title: {
+      marginBottom: theme.spacing.xl * 1.5,
+      fontFamily: `WhichWay`,
+
+      [BREAKPOINT]: {
+        marginBottom: theme.spacing.xl,
+      },
+    },
+
+    control: {
+      [BREAKPOINT]: {
+        flex: 1,
+      },
+    },
+
+    dialog: {
+    },
+    toggler: {
+
+      padding: 0,
+      width: 130,
+      paddingRight: 0,
+      border: 'none',
+      backgroundColor: '#00000000',
+    },
+  };
+});
+
+
+export function Togglable() {
+  const { classes } = useStyles();
+  const [opened, setOpened] = useState(false);
+
+  const toggl: CSSObject = {
+    display: 'none'
+  }
+
+  const cont: CSSObject = {
+      position: 'absolute',
+      top: 0,
+      right: 0
+  }
+  const smbtn : CSSObject = {
+      position: 'relative',
+      right: -84
+  }
+
+
+  return (
+    <MediaQuery smallerThan="md" styles={toggl}>
+        <Container styles={cont}>
+      <Dialog
+        opened={!opened}
+        className={classes.toggler}
+      >
+        <MediaQuery smallerThan="md" styles={smbtn}><Button
+        leftIcon={<Phone size={20} />}
+        radius="xl"
+        size="md"
+        className={classes.button}
+        styles={{
+          rightIcon: { marginLeft: 22 },
+        }}    
+        onClick={() => setOpened((o) => !o)}
+      >
+       Kontakt
+      </Button></MediaQuery>
+      </Dialog>
+      <Dialog
+        opened={opened}
+        withCloseButton
+        onClose={() => setOpened(false)}
+        size="md"
+        radius="md"
+        className={classes.dialog}
+      >
+        <ContactIconsList variant="white"></ContactIconsList>
+        <Group align="flex-end">
+
+        </Group>
+      </Dialog>
+      </Container>
+    </MediaQuery>
+  );
+}
